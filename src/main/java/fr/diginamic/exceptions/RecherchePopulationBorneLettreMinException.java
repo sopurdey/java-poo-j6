9fb42a532697bb338f87802a08927859b1ccbdbc
sopurdey <a_purdey@hotmail.com>
2021-11-23 17:21:30 +0100

    TP Exceptions : Reflection maj

---
+++ src/main/java/fr/diginamic/exceptions/RecherchePopulationBorneLettreMinException.java
@@ -0,0 +1,8 @@
+package fr.diginamic.exceptions;
+
+public class RecherchePopulationBorneLettreMinException extends Exception {
+
+	public RecherchePopulationBorneLettreMinException(String msg) {
+		super(msg);
+	}
+}
